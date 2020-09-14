d11c18e3ee5400b70d295cf55b25ed778048d2a1
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-09-14 12:53:06 -0400

    new component to show loading message

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { getUserSettings } from "./utils/loginDataManager";
 import { getOr } from "lodash/fp";
 
@@ -10,7 +9,6 @@ const lng = getOr('en-US','language',getUserSettings()) ;
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(Backend)
-  .use(LanguageDetector)
   .init({
     lng,
     debug: false,
