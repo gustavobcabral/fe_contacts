0d1523dec5c833cd57203314d47008e665698c6e
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-09-14 12:59:05 -0400

    new component to show loading message

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Spinner, Container } from "react-bootstrap";
+import { Spinner, Container, Row, Col } from "react-bootstrap";
 
 const Loading = () => (
   <Container fluid>
-    <div>loading...</div>
-    <Spinner animation="grow" variant="primary" />
-    <Spinner animation="grow" variant="secondary" />
-    <Spinner animation="grow" variant="success" />
-    <Spinner animation="grow" variant="danger" />
-    <Spinner animation="grow" variant="warning" />
-    <Spinner animation="grow" variant="info" />
-    <Spinner animation="grow" variant="light" />
-    <Spinner animation="grow" variant="dark" />
+    <Row className="justify-content-md-center">
+      <Col xs={12}>
+        <div>loading...</div>
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+        <Spinner animation="grow" variant="danger" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="info" />
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="grow" variant="dark" />
+      </Col>
+    </Row>
   </Container>
 );
 
-export default Loading
+export default Loading;
