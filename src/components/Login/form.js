05a2050e7fbed886d4e06a892a60304a0c82d21d
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-09-14 20:36:07 -0400

    validation on form login

--- src/components/Login/form.js
+++ src/components/Login/form.js
@@ -1,44 +1,62 @@
 import React from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { patternEmail } from "../../utils/forms";
 
 const FormLogin = (props) => {
-  const { t } = useTranslation(['login', 'common']);
+  const { t } = useTranslation(["login", "common"]);
   return (
     <Modal show={props.show} onHide={props.onHide} size="lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title>{t('titleModal')}</Modal.Title>
+        <Modal.Title>{t("titleModal")}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={props.onSubmit}>
+        <Form
+          noValidate
+          validated={props.state.validated}
+          onSubmit={props.onSubmit}
+        >
           <Form.Group>
-            <Form.Label>Email</Form.Label>
+            <Form.Label htmlFor="email">Email</Form.Label>
             <Form.Control
+              required
               type="email"
               name="email"
-              placeholder={t('emailPlaceHolder')}
+              placeholder={t("emailPlaceHolder")}
+              pattern={patternEmail}
               value={props.state.form.email}
               onChange={props.handleInputChange}
             />
+            <Form.Control.Feedback type="invalid">
+              {t("common:requiredFieldAndPatternValid")}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group>
-            <Form.Label>{t('passwordLabel')}</Form.Label>
+            <Form.Label>{t("passwordLabel")}</Form.Label>
             <Form.Control
+              required
               type="password"
-              placeholder={t('passwordPlaceHolder')}
+              placeholder={t("passwordPlaceHolder")}
               name="password"
               value={props.state.form.password}
               onChange={props.handleInputChange}
             />
+            <Form.Control.Feedback type="invalid">
+              {t("common:requiredField")}
+            </Form.Control.Feedback>
           </Form.Group>
-          <Button variant="primary" type="submit">
-          {t('btnSubmit')}
+          <Button
+            disabled={props.state.submitting}
+            variant="primary"
+            type="submit"
+          >
+            {t(props.state.submitting ? "common:btnSubmitting" : "btnSubmit")}
           </Button>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>{t('common:close')}</Button>
+        <Button onClick={props.onHide}>{t("common:close")}</Button>
       </Modal.Footer>
     </Modal>
   );
--- src/utils/forms.js
+++ src/utils/forms.js
@@ -1,14 +1,90 @@
-export const unformatDate = (date) =>{
+import {
+  isEmpty,
+  every,
+  get,
+  pipe,
+  flatMap,
+  flatten,
+  forEach,
+  map,
+} from "lodash/fp";
+
+export const unformatDate = (date) => {
   const split = date.slice(0, 10).split("-");
   return `${split[2]}/${split[1]}/${split[0]}`;
-}
+};
 
 export const formatDate = (date) => {
   const split = date.split("/");
   return `${split[2]}-${split[1]}-${split[0]}`;
-}
+};
+
+export const patternEmail = `^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$`;
 
 export const isValidEmail = (email) => {
-  let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(String(email).toLowerCase());
+};
+
+export function getValidation({ form, fields, submitted }) {
+  let state = {};
+
+  pipe(
+    forEach((it) => {
+      state[it.key] = map(
+        (validator) => ({
+          isValid: validator.isValid(form[it.key], form),
+          message: validator.message,
+        }),
+        it.validators
+      );
+    })
+  )(fields);
+
+  return {
+    state,
+    form: {
+      submitted,
+      isValid: isFormValid(state),
+    },
+  };
+}
+
+export function getValidationClass({ validation }) {
+  if (validation && validation.field) {
+    const isInvalid = isFieldInvalid({ validation, field: validation.field });
+    return get("form.submitted", validation)
+      ? {
+          "is-invalid": isInvalid,
+          "is-valid": !isInvalid,
+        }
+      : null;
+  } else return {};
+}
+
+export function isFormValid(state) {
+  return pipe(
+    flatMap((it) => it),
+    flatten,
+    every((it) => Boolean(it.isValid))
+  )(state);
+}
+
+export function isFieldInvalid({ validation, field }) {
+  return (
+    get("form.submitted", validation) &&
+    !validateFieldState({ validation, field })
+  );
+}
+
+export function validateFieldState({ validation, field, form, optional }) {
+  const isSubmitted = get("form.submitted", validation);
+
+  if (!isSubmitted || !mustValidate({ field, optional, form })) {
+    return null;
+  }
+}
+
+export function mustValidate({ field, form, optional }) {
+  return !optional || (optional && !isEmpty(get(field, form)))
 }
