546c07a58720015f616b02bd5c24dfd8be31c8a1
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-10-14 18:55:29 -0300

    new component to list all status traduced

--- src/components/DetailsContact/URL/NewDetailsContact.jsx
+++ src/components/DetailsContact/URL/NewDetailsContact.jsx
@@ -5,7 +5,7 @@ import SimpleReactValidator from "simple-react-validator";
 import { getOr, map, pick, get } from "lodash/fp";
 import FormDetails from "./FormDetails";
 import { getLocale, handleInputChangeGeneric } from "../../../utils/forms";
-import { details, publishers, status } from "../../../services";
+import { details, publishers } from "../../../services";
 import Swal from "sweetalert2";
 
 const fields = {
@@ -41,43 +41,22 @@ class NewDetailsContact extends React.Component {
       getOr([], "data.data", publishers)
     );
 
-  reduceStatus = (status) =>
-    map(
-      (status) => ({ value: status.id, label: status.description }),
-      getOr([], "data.data", status)
-    );
-
   async componentDidMount() {
     this.setState({ loading: true });
     const publishersOptions = this.reducePublishers(await publishers.getAll());
-    const statusOptions = this.reduceStatus(await status.getAll());
-    
 
     this.setState({
       publishersOptions,
-      statusOptions,
       loading: false,
     });
   }
 
-  setFormData = (name, value) => {
-    const { form } = this.state;
-    this.setState({
-      form: {
-        ...form,
-        [name]: value,
-      },
-    });
-  };
-
   handleInputChange(obj) {
-    const {
-      target: { name, value },
-    } = obj;
-    this.setFormData(name, value);
+    handleInputChangeGeneric(obj, this);
   }
 
   async handleSubmit() {
+    this.setState({ validated: true });
     if (!this.validator.allValid()) {
       this.validator.showMessages();
       return true;
@@ -87,12 +66,11 @@ class NewDetailsContact extends React.Component {
     const { form } = this.state;
     const { history } = this.props;
     const { t } = this.props;
-    const id = getOr(0, "props.match.params.id", this);
 
     const data = {
       detailsContact: {
         ...pick(["idPublisher", "information"], form),
-        phoneContact : getOr(0, "props.match.params.phone", this)
+        phoneContact: getOr(0, "props.match.params.phone", this),
       },
       contact: {
         idStatus: get("idStatus", form),
@@ -102,27 +80,33 @@ class NewDetailsContact extends React.Component {
       },
     };
     try {
-      const eoq = await details.create(data);
-      console.log(eoq, "EOQ MERDA")
+      await details.create(data);
       this.setState({ submitting: false });
+      history.goBack();
       Swal.fire({
         title: t("common:dataSuccessfullySaved"),
         icon: "success",
-      }).then(() => {
-        history.goBack();
       });
     } catch (error) {
-       this.setState({ submitting: false });
+      this.setState({ submitting: false });
       Swal.fire({
         icon: "error",
-        title: t("common:dataFailedSaved"),
+        title: t(
+          `common:${getOr("errorTextUndefined", "response.data.cod", error)}`
+        ),
+        text: t(
+          `common:${getOr(
+            "errorWithoutDetails",
+            "response.data.error.code",
+            error
+          )}`
+        ),
       });
     }
   }
 
   render() {
     const { t } = this.props;
-    // const { data } = this.state;
     const phone = getOr(0, "props.match.params.phone", this);
     return (
       <ContainerCRUD title={t("title")} {...this.props}>
