84e9a470a002873a9fceaef6ea2e2210a591eece
Gustavo Cabral <gustavobcabral@hotmail.com>
2020-10-15 23:21:48 -0300

    WIP: crud-contacts

--- src/services/modules/contacts.js
+++ src/services/modules/contacts.js
@@ -2,10 +2,12 @@ import api from '../api'
 
 const getAll = () => api.get('/contacts')
 
+const getOne = (id) => api.get(`/contacts/${id}`)
+
 const create = (data) => api.post('/contacts', data)
 
 const getSummary = () => api.get('/contacts/summary')
 
 const dellOne = (id) => api.delete(`/contacts/${id}`)
 
-export default { getAll, create, dellOne, getSummary }
+export default { getAll, getOne, create, dellOne, getSummary }
