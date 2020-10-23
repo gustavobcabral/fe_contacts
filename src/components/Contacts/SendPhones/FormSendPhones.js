bc7f4672075a9e7def8ddc456fb0a4b90bc9e6de
Gustavo Cabral <gustavobcabral@hotmail.com>
2020-10-23 12:13:29 -0300

    Improvements ModalSendPhones

--- src/components/Contacts/SendPhones/FormSendPhones.js
+++ src/components/Contacts/SendPhones/FormSendPhones.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { Button, Form, Row, Col } from "react-bootstrap";
-import { useTranslation } from "react-i18next";
-import SuperSelect from "../../common/SuperSelect/SuperSelect";
+import React from 'react'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+import SuperSelect from '../../common/SuperSelect/SuperSelect'
 
 const FormSendPhones = (props) => {
-  const { t } = useTranslation(["detailsContacts", "common", "contacts"]);
-  const { validator } = props;
+  const { t } = useTranslation([
+    'detailsContacts',
+    'common',
+    'contacts',
+    'sendPhones',
+  ])
+  const { validator } = props
   const {
     form,
     submitting,
@@ -15,35 +20,39 @@ const FormSendPhones = (props) => {
     validated,
     publishersOptions,
     phones,
-  } = props;
+  } = props
   return (
     <Form>
       <Row>
-        <Col>{phones}</Col>
-      </Row>
-      <Row>
+        <Col xs={7} scroll>
+          <h6>{t('sendPhones:numbers')}</h6>
+          <div style={{ overflowY: 'auto', maxHeight: '150px' }}>{phones}</div>
+        </Col>
         <Col>
+          <h6> {t('sendPhones:sendTo')}</h6>
           <SuperSelect
             name="idPublisher"
-            label={t("publisher")}
+            label={t('publisher')}
             validator={validator}
             validated={validated}
             value={form.idPublisher}
             options={publishersOptions}
             onChange={handleInputChange}
             rules="required"
+            size="sm"
           />
         </Col>
-      </Row>
+      </Row>{' '}
       <Button
+        style={{ marginTop: '20px' }}
         disabled={submitting}
         variant="primary"
         onClick={() => handleSubmit(onHide)}
       >
-        {t(submitting ? "common:btnSubmitting" : "common:btnSubmit")}
-      </Button>{" "}
+        {t(submitting ? 'common:btnSubmitting' : 'common:btnSubmit')}
+      </Button>{' '}
     </Form>
-  );
-};
+  )
+}
 
-export default FormSendPhones;
+export default FormSendPhones
--- src/components/Contacts/SendPhones/SendPhones.jsx
+++ src/components/Contacts/SendPhones/SendPhones.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { withTranslation } from "react-i18next";
-import OurModal from "../../common/OurModal/OurModal";
-import Swal from "sweetalert2";
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import OurModal from '../../common/OurModal/OurModal'
+import Swal from 'sweetalert2'
 import {
   getOr,
   map,
@@ -11,36 +11,36 @@ import {
   startsWith,
   join,
   replace,
-} from "lodash/fp";
-import SimpleReactValidator from "simple-react-validator";
-import { getLocale, handleInputChangeGeneric } from "../../../utils/forms";
-import { contacts, publishers } from "../../../services";
-import FormSendPhones from "./FormSendPhones";
-import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { COD_COUNTRY } from "../../../constants/settings";
+} from 'lodash/fp'
+import SimpleReactValidator from 'simple-react-validator'
+import { getLocale, handleInputChangeGeneric } from '../../../utils/forms'
+import { contacts, publishers } from '../../../services'
+import FormSendPhones from './FormSendPhones'
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { COD_COUNTRY } from '../../../constants/settings'
 const fields = {
-  idPublisher: "",
-};
+  idPublisher: '',
+}
 
 class NewContact extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       form: fields,
       submitting: false,
       loading: false,
       validated: false,
       publishersOptions: [],
-    };
+    }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.validator = new SimpleReactValidator({
       autoForceUpdate: this,
       locale: getLocale(this.props),
       element: (message) => <div className="text-danger">{message}</div>,
-    });
+    })
   }
 
   reducePublishers = (publishers) =>
@@ -50,106 +50,106 @@ class NewContact extends React.Component {
         label: publisher.name,
         data: publisher,
       }),
-      getOr([], "data.data", publishers)
-    );
+      getOr([], 'data.data', publishers)
+    )
 
   async componentDidMount() {
-    this.setState({ loading: true });
-    const publishersOptions = this.reducePublishers(await publishers.getAll());
+    this.setState({ loading: true })
+    const publishersOptions = this.reducePublishers(await publishers.getAll())
 
     this.setState({
       publishersOptions,
       loading: false,
-    });
+    })
   }
 
   handleInputChange(event) {
-    handleInputChangeGeneric(event, this);
+    handleInputChangeGeneric(event, this)
   }
 
   getPhonePublisher(idPublisher) {
-    const { publishersOptions } = this.state;
+    const { publishersOptions } = this.state
     const phone = pipe(
       find((publisher) => publisher.value === idPublisher),
-      getOr(0, "data.phone"),
-      (data) => (startsWith("0", data) ? data.substr(1) : data)
-    )(publishersOptions);
-    return phone.indexOf(COD_COUNTRY) === -1 ? `${COD_COUNTRY}${phone}` : phone;
+      getOr(0, 'data.phone'),
+      (data) => (startsWith('0', data) ? data.substr(1) : data)
+    )(publishersOptions)
+    return phone.indexOf(COD_COUNTRY) === -1 ? `${COD_COUNTRY}${phone}` : phone
   }
 
   parsePhonesToBeEasierToRead() {
-    const { phones } = this.props;
+    const { phones } = this.props
     return pipe(
-      join(","),
-      replace(",", "\n"),
-      (data) => "\n\n" + data,
+      join(','),
+      replace(',', '\n'),
+      (data) => '\n\n' + data,
       encodeURIComponent
-    )(phones);
+    )(phones)
   }
 
   sendMessage() {
-    const { t } = this.props;
-    const { form } = this.state;
-    const idPublisher = get("idPublisher", form);
-    const publisherPhone = this.getPhonePublisher(idPublisher);
+    const { t } = this.props
+    const { form } = this.state
+    const idPublisher = get('idPublisher', form)
+    const publisherPhone = this.getPhonePublisher(idPublisher)
     const textToSend = `${encodeURIComponent(
-      t("messageToSend")
-    )}: ${this.parsePhonesToBeEasierToRead()} `;
-    window.open(`https://wa.me/${publisherPhone}?text=${textToSend}`);
+      t('messageToSend')
+    )}: ${this.parsePhonesToBeEasierToRead()} `
+    window.open(`https://wa.me/${publisherPhone}?text=${textToSend}`)
   }
 
   async handleSubmit(onHide) {
-    this.setState({ validated: true });
+    this.setState({ validated: true })
 
     if (!this.validator.allValid()) {
-      this.validator.showMessages();
-      return true;
+      this.validator.showMessages()
+      return true
     }
-    this.setState({ submitting: true });
+    this.setState({ submitting: true })
 
-    const { form } = this.state;
-    const { t, phones } = this.props;
-    const idPublisher = get("idPublisher", form);
+    const { form } = this.state
+    const { t, phones } = this.props
+    const idPublisher = get('idPublisher', form)
 
     const data = {
       phones,
       idPublisher,
-    };
+    }
 
     try {
-      await contacts.assign(data);
-      this.sendMessage();
-      this.setState({ submitting: false });
+      await contacts.assign(data)
+      this.sendMessage()
+      this.setState({ submitting: false })
       Swal.fire({
-        title: t("common:dataSuccessfullySaved"),
-        icon: "success",
+        title: t('common:dataSuccessfullySaved'),
+        icon: 'success',
         timer: 2000,
         timerProgressBar: true,
-      });
-      onHide();
-      this.setState({ form: fields, submitting: false, validated: false });
-      this.validator.hideMessages();
+      })
+      onHide()
+      this.setState({ form: fields, submitting: false, validated: false })
+      this.validator.hideMessages()
     } catch (error) {
-      this.setState({ submitting: false });
+      this.setState({ submitting: false })
       Swal.fire({
-        icon: "error",
+        icon: 'error',
         title: t(
-          `common:${getOr("errorTextUndefined", "response.data.cod", error)}`
+          `common:${getOr('errorTextUndefined', 'response.data.cod', error)}`
         ),
         text: t(
           `common:${getOr(
-            "errorWithoutDetails",
-            "response.data.error.code",
+            'errorWithoutDetails',
+            'response.data.error.code',
             error
           )}`
         ),
-      });
+      })
     }
   }
 
   render() {
-    const { form, validated, publishersOptions } = this.state;
-    const { t, phones } = this.props;
+    const { form, validated, publishersOptions } = this.state
+    const { t, phones } = this.props
     return (
       <OurModal
         body={FormSendPhones}
@@ -158,15 +158,15 @@ class NewContact extends React.Component {
         handleSubmit={this.handleSubmit}
         handleInputChange={this.handleInputChange}
         form={form}
-        phones={join(", ", phones)}
+        phones={join(', ', phones)}
         publishersOptions={publishersOptions}
-        title={`${t("common:new")} ${t("titleCrud")}`}
+        title={`${t('title')}`}
         buttonText={<FontAwesomeIcon icon={faShareAlt} />}
         buttonDisabled={phones.length === 0}
         buttonVariant="warning"
       />
-    );
+    )
   }
 }
 
-export default withTranslation(["SendPhones", "common"])(NewContact);
+export default withTranslation(['SendPhones', 'common'])(NewContact)
