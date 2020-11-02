e28154509dbbf3e0ed0c2a64780eaee51fe87df5
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-11-02 07:18:34 -0300

    deploy to heroku

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import Routes from "./routes/routes";
-import Loading from "./components/common/loading/Loading"
+import Loading from "./components/common/loading/loading"
 
 
 const App = () => (
