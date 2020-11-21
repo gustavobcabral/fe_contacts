bc53b0bc0e8149b781bc54564bbddd65e7c6a81f
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-11-21 15:41:57 -0300

    fix bugs loading

--- src/components/Publishers/FormPublisher.js
+++ src/components/Publishers/FormPublisher.js
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import CheckNumber from "../common/CheckNumbers/CheckNumbers";
 import ResponsibilitySelect from "../common/ResponsibilitySelect/ResponsibilitySelect";
 import SuperFormControl from "../common/SuperFormControl/SuperFormControl";
+import ReactPlaceholder from "react-placeholder";
 
 const FormPublishers = (props) => {
   const { t } = useTranslation([
@@ -16,109 +17,116 @@ const FormPublishers = (props) => {
   const {
     form,
     validator,
-    submitting,
+    loading,
     handleSubmit,
     onHide,
     handleInputChange,
     validated,
   } = props;
   return (
-    <Form>
-      <Row>
-        <Col xs={12} lg={6}>
-          <SuperFormControl
-            type="text"
-            name="name"
-            label={t("name")}
-            validator={validator}
-            validated={validated}
-            value={form.name}
-            onChange={handleInputChange}
-            disabled={form.disabled}
-            rules="required"
-          />
-        </Col>
-        <Col xs={12} lg={6}>
-          <SuperFormControl
-            type="number"
-            name="phone"
-            label={t("phone")}
-            endLabel={<CheckNumber phone={form.phone} />}
-            validator={validator}
-            validated={validated}
-            value={form.phone}
-            onChange={handleInputChange}
-            rules="required|min:10"
-            disabled={form.disabled}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} lg={6}>
-          <SuperFormControl
-            type="email"
-            name="email"
-            label={t("email")}
-            validator={validator}
-            validated={validated}
-            value={form.email}
-            onChange={handleInputChange}
-            rules="email"
-            disabled={form.disabled}
-          />
-        </Col>
-        <Col xs={12} lg={6}>
-          <ResponsibilitySelect
-            name="idResponsibility"
-            label={t("responsibility")}
-            validator={validator}
-            validated={validated}
-            value={form.idResponsibility}
-            onChange={handleInputChange}
-            justAllowedForMe={form.justAllowedForMe}
-            rules="required"
-            disabled={form.disabled}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} lg={6}>
-          <SuperFormControl
-            type="password"
-            name="password"
-            label={t("password")}
-            validator={validator}
-            validated={validated}
-            placeholder={t("password")}
-            value={form.password}
-            onChange={handleInputChange}
-            disabled={form.disabled}
-          />
-        </Col>
-        <Col xs={12} lg={6}>
-          <SuperFormControl
-            type="password"
-            name="repeatPassword"
-            label={t("repeatPasswordLabel")}
-            validator={validator}
-            validated={validated}
-            placeholder={t("repeatPasswordPlaceHolder")}
-            value={form.repeatPassword}
-            onChange={handleInputChange}
-            rules="mustBeEqualFieldPassword"
-            extraRules={form.password}
-            disabled={form.disabled}
-          />
-        </Col>
-      </Row>
-      <Button
-        disabled={form.disabled || submitting}
-        variant="primary"
-        onClick={() => handleSubmit(onHide)}
-      >
-        {t(submitting ? "common:btnSubmitting" : "common:btnSubmit")}
-      </Button>{" "}
-    </Form>
+    <ReactPlaceholder
+      showLoadingAnimation={true}
+      type="text"
+      ready={!loading}
+      rows={12}
+    >
+      <Form>
+        <Row>
+          <Col xs={12} lg={6}>
+            <SuperFormControl
+              type="text"
+              name="name"
+              label={t("name")}
+              validator={validator}
+              validated={validated}
+              value={form.name}
+              onChange={handleInputChange}
+              disabled={form.disabled}
+              rules="required"
+            />
+          </Col>
+          <Col xs={12} lg={6}>
+            <SuperFormControl
+              type="number"
+              name="phone"
+              label={t("phone")}
+              endLabel={<CheckNumber phone={form.phone} />}
+              validator={validator}
+              validated={validated}
+              value={form.phone}
+              onChange={handleInputChange}
+              rules="required|min:10"
+              disabled={form.disabled}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} lg={6}>
+            <SuperFormControl
+              type="email"
+              name="email"
+              label={t("email")}
+              validator={validator}
+              validated={validated}
+              value={form.email}
+              onChange={handleInputChange}
+              rules="email"
+              disabled={form.disabled}
+            />
+          </Col>
+          <Col xs={12} lg={6}>
+            <ResponsibilitySelect
+              name="idResponsibility"
+              label={t("responsibility")}
+              validator={validator}
+              validated={validated}
+              value={form.idResponsibility}
+              onChange={handleInputChange}
+              justAllowedForMe={form.justAllowedForMe}
+              rules="required"
+              disabled={form.disabled}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} lg={6}>
+            <SuperFormControl
+              type="password"
+              name="password"
+              label={t("password")}
+              validator={validator}
+              validated={validated}
+              placeholder={t("password")}
+              value={form.password}
+              onChange={handleInputChange}
+              disabled={form.disabled}
+            />
+          </Col>
+          <Col xs={12} lg={6}>
+            <SuperFormControl
+              type="password"
+              name="repeatPassword"
+              label={t("repeatPasswordLabel")}
+              validator={validator}
+              validated={validated}
+              placeholder={t("repeatPasswordPlaceHolder")}
+              value={form.repeatPassword}
+              onChange={handleInputChange}
+              rules="mustBeEqualFieldPassword"
+              extraRules={form.password}
+              disabled={form.disabled}
+            />
+          </Col>
+        </Row>
+        <Button
+          disabled={form.disabled || loading}
+          variant="primary"
+          onClick={() => handleSubmit(onHide)}
+        >
+          {t(loading ? "common:btnSubmitting" : "common:btnSubmit")}
+        </Button>{" "}
+      </Form>
+    </ReactPlaceholder>
   );
 };
 
--- src/components/common/GenderSelect/GenderSelect.jsx
+++ src/components/common/GenderSelect/GenderSelect.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SuperSelect from "../SuperSelect/SuperSelect";
 import { useTranslation } from "react-i18next";
+import { orderBy } from "lodash/fp";
 
 const GenderSelect = (props) => {
   const { t } = useTranslation(["contacts", "common"]);
@@ -8,11 +9,15 @@ const GenderSelect = (props) => {
   const { validator } = props;
   const { value, onChange, validated } = props;
 
-  const genderOptions = [
-    { label: t("unknown"), value: "unknown" },
-    { label: t("male"), value: "male" },
-    { label: t("female"), value: "female" },
-  ];
+  const genderOptions = orderBy(
+    ["label"],
+    ["asc"],
+    [
+      { label: t("unknown"), value: "unknown" },
+      { label: t("male"), value: "male" },
+      { label: t("female"), value: "female" },
+    ]
+  );
 
   return (
     <SuperSelect
