d7db12bb97b09f67de35f7cee036e45722cb25cb
Rodrigo Pereira <pereirapy@hotmail.com>
2021-03-19 11:27:24 -0300

    new filter on waiting feedback list - created by

--- src/components/common/SuperSelect/SuperSelect.jsx
+++ src/components/common/SuperSelect/SuperSelect.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
 import Select from 'react-select'
-import { find } from 'lodash/fp'
+import { find, map, includes } from 'lodash/fp'
 import './style-super-select.css'
 import ReactPlaceholder from 'react-placeholder'
 import { withTranslation } from 'react-i18next'
@@ -19,9 +19,10 @@ const SuperSelect = (props) => {
     rules,
     disabled = false,
     loading = false,
+    isMulti = false,
     rows = 2,
     t,
-    placeHolderSelect = 'placeHolderSelect'
+    placeHolderSelect = 'placeHolderSelect',
   } = props
 
   const [touched, setTouched] = React.useState(false)
@@ -30,6 +31,46 @@ const SuperSelect = (props) => {
     setTouched(true)
     //validator.showMessageFor(name);
   }
+
+  const setValue = () => {
+    if (isMulti) {
+      return (
+        value &&
+        options &&
+        map((option) => {
+          if (includes(option.value, value)) return option
+        }, options)
+      )
+    } else {
+      return (
+        value &&
+        value !== '' &&
+        options &&
+        find((option) => option.value === value, options)
+      )
+    }
+  }
+  const setOnChange = (paramValues) => {
+    if (isMulti) {
+      const arrayOnlyValues = map((option) => option.value, paramValues)
+      return onChange({
+        target: { name, value: paramValues ? arrayOnlyValues : [] },
+      })
+    } else {
+      return onChange({
+        target: { name, value: paramValues ? paramValues.value : '' },
+      })
+    }
+  }
+
+  const setClassName = () => {
+    return (validated || touched) && rules && !validator.fieldValid(name)
+      ? 'is-invalid'
+      : (validated || touched) &&
+        ((rules && validator.fieldValid(name)) || !rules)
+      ? 'is-valid'
+      : ''
+  }
   return (
     <ReactPlaceholder
       showLoadingAnimation={true}
@@ -37,35 +78,19 @@ const SuperSelect = (props) => {
       ready={!loading}
       rows={rows}
     >
-      <Form.Group
-        controlId={name}
-        className={
-          (validated || touched) && rules && !validator.fieldValid(name)
-            ? 'is-invalid'
-            : (validated || touched) &&
-              ((rules && validator.fieldValid(name)) || !rules)
-            ? 'is-valid'
-            : ''
-        }
-      >
+      <Form.Group controlId={name} className={setClassName()}>
         <Form.Label>{label}</Form.Label>
         <Select
           name={name}
-          value={
-            value &&
-            value !== '' &&
-            options &&
-            find((option) => option.value === value, options)
-          }
+          value={setValue()}
           options={options}
           isClearable={isClearable || false}
           placeholder={t(placeHolderSelect)}
           onBlur={onBlurLocal}
-          onChange={(obj) =>
-            onChange({ target: { name, value: obj ? obj.value : '' } })
-          }
+          onChange={setOnChange}
           classNamePrefix="react-select"
           isDisabled={disabled}
+          isMulti={isMulti}
         />
         {rules && validator.message(name, value, rules)}
       </Form.Group>
